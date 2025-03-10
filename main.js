dbf22df048e93a1451abee5ed9277e469aad9f00
Phan Quang Huy <huycris567@gmail.com>
2025-03-10 10:32:14 +0700

    Cập nhật code mới

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-var name="me may beo vcl"
-alert(name)
+// function alo(){
+//     alert("em oi em oi em oi em oi");
+// }
+function jvay(){
+    var xd=""
+    for( var i of arguments){
+        xd+=`${i} - `
+    }
+    console.log(xd);
+}
+
+jvay("memaybep",1,2,3)
